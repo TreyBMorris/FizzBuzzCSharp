772d1c935e1cf52219cb0392dea5f0dad8c7276e
BuildTools <unconfigured@null.spigotmc.org>
2020-04-12 21:53:35 -0400

    Commit to Program

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberToReach = 100;
+            
+            for(int i = 1; i<=numberToReach; i++)
+            {
+                Console.WriteLine(i);
+
+                Boolean DivisiblebyThree = i % 3 == 0;
+                Boolean DivisiblebyFive = i % 5 == 0;
+
+                if (DivisiblebyThree)
+                {
+                    Console.WriteLine(" Fizz");
+                }
+                if (DivisiblebyFive)
+                {
+                    Console.WriteLine(" Buzz");
+                }
+               Console.WriteLine();
+            }
+
+        }
+    }
+}
